7d45f22d63a30b7631baa72a8497db8c2b1ffb37
Olena Ageyeva <eageeva@hotmail.com>
2018-05-14 12:34:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function tailsNeverFails(){
+  let count=0;
+  while(Math.random() >= 0.5){
+    count++;
+  }
+  return("You got "+count+" tails in a row!");
+}
+
+function printBadges(arr){
+ for(let i=1; i<arr.length+1; i++){
+    console.log('Welcome '+arr[i-1]+'! You are employee #'+i+'.');
+ }
+ return arr
+}
